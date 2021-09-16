d2003a635838816ebffb7dc28f40efab89f18b5a
JKohls12 <89868836+JKohls12@users.noreply.github.com>
2021-09-16 14:22:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ function changeRange() {
   var r = parseInt(document.getElementById("R").value);
   var g = parseInt(document.getElementById("G").value);
   var b = parseInt(document.getElementById("B").value);
-  var square = document.getElementByClassName("square");
   
   var RGB = "rgb(" + r + ", " + g + ", " + b + ")";
-  var color = "(" + 1/r + ", " + 1/g + ", " + 1/b + ")";
+  var color = "(" + ((1 / r)*r) + ", " + ((1 / g)*g) + ", " + ((1 / b)*b) + ")";
   
   document.getElementById("hues-label").innerText = color
   document.getElementById("R").innerText = r
   document.getElementById("G").innerText = g
   document.getElementById("B").innerText = b
-  square = RGB;
+
 }
