c360b132afca114302a838075ffbeb5ee80b369c
JKohls12 <89868836+JKohls12@users.noreply.github.com>
2021-09-16 13:00:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ let RGBInput = document.querySelector('#RGB');
 colorInput.addEventListener('input', () => {
   let color = colorInput.value;
   RGBInput.value = color;
-  document.body.style.backgroundColor = color;
+  //document.body.style.backgroundColor = color;
 });
