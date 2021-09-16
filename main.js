aa165d73469551df1a3a50c8693cb5e9b6878fea
JKohls12 <89868836+JKohls12@users.noreply.github.com>
2021-09-16 14:03:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ function changeRange() {
   var g = parseInt(document.getElementById("G").value);
   var b = parseInt(document.getElementById("B").value);
   var colorpicker = document.querySelector("colorpicker");
+  var square = document.getElementByClassName("square");
   
   var RGB = "rgb(" + r + ", " + g + ", " + b + ")";
   var color = "(" + 1/r + ", " + 1/g + ", " + 1/b + ")";
   
-  
+  square = RGB;
   colorpicker = RGB;
 }
