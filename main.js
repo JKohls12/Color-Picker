8d8c7ea33408340101a024d8c61d48b65f8fd4fb
JKohls12 <89868836+JKohls12@users.noreply.github.com>
2021-09-17 19:56:43 -0500

    This commit will add light into the RGB conversion

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ function changeRange() {
   var g = parseInt(document.getElementById("G").value);
   var b = parseInt(document.getElementById("B").value);
   var equation = (1/(r+g+b));
+  var light = (1/equation)* equation;
   
   var RGB = "rgb(" + r + ", " + g + ", " + b + ")";
-  var color = "(" + Math.round((equation*r)*10) / 10 + ", " + Math.round((equation*g)* 10) / 10 + ", " + Math.round((equation*b) * 10) / 10 + ")";
+  var color = "(" + Math.round((equation*r)*10) / 10 + ", " + Math.round((equation*g)* 10) / 10 + ", " + Math.round((equation*b) * 10) / 10 + ", " + light + ")";
   
   document.getElementById("hues-label").innerText = color;
   document.getElementById("Rlabel").innerText = r; 
