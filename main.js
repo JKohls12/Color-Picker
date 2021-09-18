79adb30e7a2b7fa0a69f5657dff9d9df34581f76
JKohls12 <89868836+JKohls12@users.noreply.github.com>
2021-09-17 20:11:21 -0500

    This commit adds rounding to the light conversion

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function changeRange() {
   var light = (1/equation)* equation;
   
   var RGB = "rgb(" + r + ", " + g + ", " + b + ")";
-  var color = "(" + Math.round((equation*r)*10) / 10 + ", " + Math.round((equation*g)* 10) / 10 + ", " + Math.round((equation*b) * 10) / 10 + ", " + light + ")";
+  var color = "(" + Math.round((equation*r)*10) / 10 + ", " + Math.round((equation*g)* 10) / 10 + ", " + Math.round((equation*b) * 10) / 10 + ", " + Math.round(light) + ")";
   
   document.getElementById("hues-label").innerText = color;
   document.getElementById("Rlabel").innerText = r; 
